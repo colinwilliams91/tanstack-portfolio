3704be4f3899f3436ecfca6daa7cba7b347af529
colin <colin.williams.dev@gmail.com>
2025-11-29 04:08:31 -0600

    :memo: outside of scope, docs on types contracts

--- src/types/projects/index.ts
+++ src/types/projects/index.ts
@@ -1,10 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////////
-/// This file handles type definitions related to projects throughout the application.
-/// e.g. DTOs, API Contracts
-///
-/// Domain level types/interfaces are defined in co-located "abstract" directories.
-/// e.g. ~/src/components/projects/abstract
-/////////////////////////////////////////////////////////////////////////////////////
 
 export interface Project {
   id: number;
