729eb60f73112c914115847f02563ca770d83f67
colin <colin.williams.dev@gmail.com>
2025-11-28 22:08:08 -0600

    :memo: docs for types DTO and API contract layer

--- src/types/projects/index.ts
+++ src/types/projects/index.ts
@@ -1,3 +1,10 @@
+//////////////////////////////////////////////////////////////////////////////////////
+/// This file handles type definitions related to projects throughout the application.
+/// e.g. DTOs, API Contracts
+///
+/// Domain level types/interfaces are defined in co-located "abstract" directories.
+/// e.g. ~/src/components/projects/abstract
+/////////////////////////////////////////////////////////////////////////////////////
 
 export interface Project {
   id: number;
