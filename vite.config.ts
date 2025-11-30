cb7198a5ca54a3df4066ffdf1dcdcdbc66b66098
colin <colin.williams.dev@gmail.com>
2025-11-29 23:21:40 -0600

    :wrench: upgrade tailwindcss, tailwindcss/vite, vite, daisyui to latest and update syntax, clean install

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import viteReact from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite';
 // import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
     }),
     tanstackStart(),
     viteReact(),
+    tailwindcss(),
     // netlify(), // uncomment for local netlify edge function simulation
   ],
 });
