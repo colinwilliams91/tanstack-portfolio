ce2621af68f2a7c5d3f6a1dac5f7feb4e3dc7d0b
colin <colin.williams.dev@gmail.com>
2025-12-16 19:55:19 +0700

    :sparkles: first call GH api res working, TODO loader is disabled for TS error, fetch has logs, i dont think query needs key builder

--- src/handlers/fetch/projects.ts
+++ src/handlers/fetch/projects.ts
@@ -1,19 +1,23 @@
-import { type Project } from "~/types/projects/index";
+import { API_URLS } from "~/constants/public-api-urls";
+import { type GitHubRepositoriesResponse } from "~/types/projects/index";
 
 // TODO: remove DEV environment check and diagnostic logging
 
-export async function fetchProjects(): Promise<Project[]> {
+export async function fetchProjects(): Promise<GitHubRepositoriesResponse> {
   if (import.meta.env.DEV) {
     console.log("[fetchProjects] Fetching projects data...");
   }
   const startTime = performance.now();
 
-  const data = await import("~/data/projects").then(module => module.projects);
+  // TODO: remove fallback/dummy local data once we have component to handle empty response/loading state
+  // const data = await import("~/data/projects").then(module => module.projects);
+  const res = await fetch(API_URLS.GITHUB.USER_REPOS);
+  const data = await res.json() satisfies GitHubRepositoriesResponse;
 
   if (import.meta.env.DEV) {
     const endTime = performance.now();
     console.log(`[fetchProjects] Fetched ${data.length} projects in ${(endTime - startTime).toFixed(2)}ms`);
   }
-
+  console.dir(data);
   return data;
 }
--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -5,7 +5,7 @@ import { ProjectsContainerPage } from "~c/projects/list/projects.container";
 import { QUERY_OPTIONS } from "~/constants/query-options";
 
 export const Route = createFileRoute("/projects/")({
-  loader: () =>
-    queryClient.ensureQueryData(QUERY_OPTIONS.PROJECTS.LIST),
+  // loader: () =>
+  //   queryClient.ensureQueryData(QUERY_OPTIONS.PROJECTS.LIST),
   component: ProjectsContainerPage,
 });
