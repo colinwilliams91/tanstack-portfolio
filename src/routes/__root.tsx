e04621d97d893d2d62cb3d567d8f595b59ea0ef4
colin <colin.williams.dev@gmail.com>
2025-12-17 17:03:10 +0700

    :wrench: footer element cannot be child of html element fix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -59,16 +59,16 @@ function RootDocument({ children }: { children: ReactNode }) {
         </QueryClientProvider>
         <Scripts />
         <TanStackRouterDevtools position="bottom-right" />
+        <footer className="container mx-auto px-6 py-6 opacity-60 text-xs text-center mt-auto">
+          <div>
+            ｡˚☁️©️ colin williams • {new Date().getFullYear()} ˚｡˚
+          </div>
+          <div>
+            ˚➶ ｡˚ ☁️build: {__BUILD_VERSION__}
+            {__BUILD_COMMIT__ ? ` · ${__BUILD_COMMIT__}` : ''} ˋ°•*⁀➷
+          </div>
+        </footer>
       </body>
-      <footer className="container mx-auto px-6 py-6 opacity-60 text-xs text-center mt-auto">
-        <div>
-          ｡˚☁️©️ colin williams • {new Date().getFullYear()} ˚｡˚
-        </div>
-        <div>
-          ˚➶ ｡˚ ☁️build: {__BUILD_VERSION__}
-          {__BUILD_COMMIT__ ? ` · ${__BUILD_COMMIT__}` : ''} ˋ°•*⁀➷
-        </div>
-      </footer>
     </html>
   );
 }
