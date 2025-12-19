a97101cd87aa6361857fa149dbc9d0f623f8fbe2
colin <colin.williams.dev@gmail.com>
2025-12-19 19:32:33 +0700

    :sparkles: functional v1 GH API res cards projects

--- src/components/projects/list/projects.presenter.tsx
+++ src/components/projects/list/projects.presenter.tsx
@@ -13,23 +13,33 @@ export function ProjectsPresenter({ data, isLoading }: ProjectsPresenterProps) {
   return (
     <div>
       <h1 className="text-3xl font-bold mb-6">Projects</h1>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {data?.map((project) => (
-          <div key={project.id} className="card bg-base-200 shadow-sm hover:shadow-md hover-3d transition-shadow">
-            <div className="card-body text-sm">
-              <h2 className="card-title">{project.name}</h2>
-              <img className="mask mask-circle w-16 h-16" src={project.owner.avatar_url} alt={project.name} />
-              <p className="text-sm p-3 bg-base-100/30
-                opacity-80 italic shadow-md rounded-box backdrop-blur-sm"
-              >{project.description}
-              </p>
-              <p>💻 {project.language?.length ? project.language : "N/A"}</p>
-              <p>✨ {project.stargazers_count}</p>
-              <p>🍴 {project.forks_count}</p>
-              <p>🗓️ {getMostRecentDate([project.updated_at, project.pushed_at])}</p>
-              <p>⚖️ {formatDataSize(project.size)}</p>
+      <div className="grid gap-4.5 md:grid-cols-2 lg:grid-cols-3">
+        {data?.slice().sort((a, b) => b.stargazers_count - a.stargazers_count).map((project) => (
+          <a href={project.url} key={project.id} className="hover-3d">
+            <div className="card bg-base-200 shadow-sm hover:shadow-md rounded-box">
+              <div className="card-body text-sm">
+                <h2 className="card-title">{project.name}</h2>
+                <img className="mask mask-circle w-16 h-16" src={project.owner.avatar_url} alt={project.name} />
+                <div className="text-sm p-3 bg-base-100/30
+                  opacity-80 italic shadow-sm rounded-box backdrop-blur-lg"
+                >{project.description}
+                </div>
+                <div>💻 {project.language?.length ? project.language : "N/A"}</div>
+                <div>✨ {project.stargazers_count}</div>
+                <div>🍴 {project.forks_count}</div>
+                <div>🗓️ {getMostRecentDate([project.updated_at, project.pushed_at])}</div>
+                <div>⚖️ {formatDataSize(project.size)}</div>
+              </div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
             </div>
-          </div>
+          </a>
         ))}
       </div>
     </div>
