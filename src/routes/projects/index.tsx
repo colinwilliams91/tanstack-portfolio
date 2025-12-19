a710395b6d34ccd4397bfb8c86f772090f861846
colin <colin.williams.dev@gmail.com>
2025-12-19 20:24:01 +0700

    :sparkles: preload repos working amazing difference

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -3,9 +3,10 @@ import { queryClient } from "~/router";
 import { ProjectsContainerPage } from "~c/projects/list/projects.container";
 
 import { QUERY_OPTIONS } from "~/constants/queries/query-options";
+import { GitHubRepositoriesResponse } from "~/types/projects";
 
 export const Route = createFileRoute("/projects/")({
-  // loader: () =>
-  //   queryClient.ensureQueryData(QUERY_OPTIONS.PROJECTS.LIST),
+  loader: (): Promise<GitHubRepositoriesResponse> =>
+    queryClient.ensureQueryData(QUERY_OPTIONS.PROJECTS.LIST),
   component: ProjectsContainerPage,
 });
