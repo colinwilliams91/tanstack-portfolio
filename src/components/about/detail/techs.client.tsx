8d44b0c13890a14995d5b6378d4941df24040eb8
colin <colin.williams.dev@gmail.com>
2025-12-22 01:07:43 +0700

    :nail_care: techs client glass rotate POC

--- src/components/about/detail/techs.client.tsx
+++ src/components/about/detail/techs.client.tsx
@@ -3,15 +3,15 @@ export const ClientTechs = () => {
     return (
         <span className="text-rotate duration-9000">
             <span>
-                <span className="glass rounded-field bg-success/25">React</span>
-                <span className="glass rounded-field">TanStack</span>
-                <span className="glass rounded-field">Svelte</span>
-                <span className="glass rounded-field">Vue.js</span>
-                <span className="glass rounded-field">jQuery</span>
-                <span className="glass rounded-field">Angular</span>
-                <span className="glass rounded-field">Next</span>
-                <span className="glass rounded-field">HTMX</span>
-                <span className="glass rounded-field">Tailwind</span>
+                <span className="glass rounded-field bg-success/25 px-3">React</span>
+                <span className="glass rounded-field px-3">TanStack</span>
+                <span className="glass rounded-field px-3">Svelte</span>
+                <span className="glass rounded-field px-3">Vue.js</span>
+                <span className="glass rounded-field px-3">jQuery</span>
+                <span className="glass rounded-field px-3">Angular</span>
+                <span className="glass rounded-field px-3">Next</span>
+                <span className="glass rounded-field px-3">HTMX</span>
+                <span className="glass rounded-field px-3">Tailwind</span>
             </span>
         </span>
     )
